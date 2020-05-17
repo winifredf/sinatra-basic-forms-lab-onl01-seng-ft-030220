f58f9694f52fd15a841a1a9179ccb39fe45ce280
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 20:09:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   post 'puppy' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @puppy = Puppy.new([:name], [:breed], [:age])
     
     erb :display_puppy
   end
