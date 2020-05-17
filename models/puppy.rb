4461985c4f2a21f56b68bc027156068d4dbec51c
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 19:49:14 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -5,10 +5,10 @@ class Puppy < Sinatra::Base
   
   @@all = []
   
-  def initialize(params)
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:age]
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
   
   def self
