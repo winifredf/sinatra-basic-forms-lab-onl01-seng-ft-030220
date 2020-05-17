37679e4d1d15a995149bb1eccd8ca213a5623836
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 19:39:03 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,9 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy < Sinatra::Base
-  
+  def puppy
+    @name = :name
+    @breed = :breed
+    @age = :age
   
 end
   
