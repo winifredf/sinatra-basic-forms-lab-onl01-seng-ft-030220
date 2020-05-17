0ba81613b2db20be065e9b6f4f3ebc19fd730128
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 19:48:26 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,7 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy < Sinatra::Base
-  attr_accessor :name, :breed, :age
+  attr_accessor :name, :age
+  attr_reader :breed
   
   @@all = []
   
